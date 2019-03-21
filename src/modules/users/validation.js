cbbe47a116726b6cc2f38d7c86aaa6cb617b538b
TheJSGirl <prinegi100@gmail.com>
2019-03-21 19:48:50 +0530

    feat(validation): added validation for sigin request

--- src/modules/users/validation.js
+++ src/modules/users/validation.js
@@ -6,7 +6,13 @@ module.exports = {
             email: Joi.string().email().required(),
             mobile: Joi.string().min(9).max(10),
             username: Joi.string().min(4).max(255).required(),
-            password: Joi.string().min(4).max(255).required()
+            password: Joi.string().min(4).max(10).required()
         }
     },
+    signin: {
+        body: {
+            email: Joi.string().email().required(),
+            password: Joi.strict().min(4).max(10).required()
+        }
+    }
 }
