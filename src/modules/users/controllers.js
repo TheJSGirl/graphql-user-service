8235a3e2554358a65d6a1e4fd4cdb69c019dd5f6
thejsgirl <priyanka.negi@wesence.com>
2019-03-21 16:36:15 +0530

    -basic  api structure for POST /signUp

--- src/modules/users/controllers.js
+++ src/modules/users/controllers.js
@@ -1,4 +1,6 @@
 const User = require('./models');
+const bcrypt = require('bcrypt');
+//const validateUser = require('./validation');
 
 
 async function get(req, res){
@@ -6,8 +8,22 @@ async function get(req, res){
 }
 
 async function registerUser(req, res) {
-    const {password} = req.bod
-    
+
+    const {name, password, email, mobile, username, image } = req.body
+
+    const hashedPassword = await bcrypt.hashSync(password);
+    const data = {
+        name,
+        password: hashedPassword,
+        email,
+        mobile,
+        username,
+        image
+    }
+
+    const userCredentials = new User(data);
+    userCredentials.save();
+    res.status(200).json(req.body);
 }
 
 module.exports = {
